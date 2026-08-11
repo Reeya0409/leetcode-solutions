2c2e71b8f07c051a5fe10e0c6c61b0cf98122d6f
Reeya Sharma <reeyasharma0904@gmail.com>
2026-08-11 23:31:53 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (70.87%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,16 +1,30 @@
+// class Solution {
+//     public int fib(int n) {
+//         int [] arr=new int [n+1];
+//         Arrays.fill(arr,-1);
+//         return fibonacci(n,arr);    
+//     }
+//     static int fibonacci(int n, int arr[]){
+//         if(n<=1){
+//             return n;
+//         }
+//         if(arr[n]==-1){
+//             arr[n]=fibonacci(n-1,arr)+fibonacci(n-2,arr);
+//         }
+//         return arr[n];
+//     }
+// }
+
+// Bottom-Up DP / Tabulation
 class Solution {
     public int fib(int n) {
-        int [] arr=new int [n+1];
-        Arrays.fill(arr,-1);
-        return fibonacci(n,arr);    
-    }
-    static int fibonacci(int n, int arr[]){
-        if(n<=1){
-            return n;
+        if(n<=1) return n;
+        int dp[]=new int[n+1];
+        dp[0]=0;
+        dp[1]=1;
+        for (int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
         }
-        if(arr[n]==-1){
-            arr[n]=fibonacci(n-1,arr)+fibonacci(n-2,arr);
-        }
-        return arr[n];
+        return dp[n];
     }
 }
