bc577bac8eee33fc5161cc7c76eea604503cf493
Reeya Sharma <reeyasharma0904@gmail.com>
2026-08-12 09:56:11 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (75.77%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -6,7 +6,21 @@ class Solution {
         if(nums.length==2){
             return Math.max(nums[0],nums[1]);
         }
-        int arr[]=new int[nums.length];
+// memoization
+//         int arr[]=new int[nums.length];
+//         arr[0]=nums[0];
+//         arr[1]=Math.max(nums[0],nums[1]);
+//         for (int i=2;i<nums.length;i++){
+//             arr[i]=Math.max(arr[i-1],(arr[i-2]+nums[i]));
+//         }
+//         return arr[nums.length-1];
+//     }
+// }
+
+
+// Tabularization
+
+int arr[]=new int[nums.length];
         arr[0]=nums[0];
         arr[1]=Math.max(nums[0],nums[1]);
         for (int i=2;i<nums.length;i++){
